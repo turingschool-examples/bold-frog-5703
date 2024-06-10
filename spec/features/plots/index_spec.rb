f7290a88b7655c6299da1388815bfef60999b273
Austin K <austink@Austins-MacBook-Pro.local>
2024-06-10 12:04:20 -0400

    US2 Test

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "Plots Index Page" do
         PlotPlant.create!(plot: @plot2, plant: @plant1)
     end
 
-    it "shows all plot numbers and plants" do
+    it "should shows all plot numbers and plants" do
         visit plots_path
 
         expect(page).to have_content("Plot Number: 1")
@@ -22,5 +22,23 @@ RSpec.describe "Plots Index Page" do
 
         expect(page).to have_content("Plot Number: 2")
         expect(page).to have_content("Tomato")
+    end
+
+    it "should remove a plant from a plot" do
+        visit plots_path
+
+        within("#plot-#{@plot1.id}") do
+            within("#plant-#{@plant1.id}") do
+                click_button "Remove"
+            end
+        end
+
+        expect(current_path).to eq(plots_path)
+        within("#plot-#{@plot.id}") do
+            expect(page).to_not have_content("Tomato")
+        end
+        within("#plot-#{@plot2.id}") do
+            expect(page).to have_content("Tomato")
+        end
     end
 end

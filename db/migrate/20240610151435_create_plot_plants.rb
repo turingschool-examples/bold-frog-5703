496ddeaf15753c6f07473e25c632b5f1fbb9d0f2
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-06-10 11:50:34 -0400

    spacing got me

--- db/migrate/20240610151435_create_plot_plants.rb
+++ db/migrate/20240610151435_create_plot_plants.rb
@@ -1,8 +1,8 @@
 class CreatePlotPlants < ActiveRecord::Migration[7.1]
   def change
     create_table :plot_plants do |t|
-      t.references :plants, null: false, foreign_key: true
-      t.references :plots, null: false, foreign_key: true
+      t.references :plant, null: false, foreign_key: true
+      t.references :plot, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -30,12 +30,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_10_151435) do
   end
 
   create_table "plot_plants", force: :cascade do |t|
-    t.bigint "plants_id", null: false
-    t.bigint "plots_id", null: false
+    t.bigint "plant_id", null: false
+    t.bigint "plot_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["plants_id"], name: "index_plot_plants_on_plants_id"
-    t.index ["plots_id"], name: "index_plot_plants_on_plots_id"
+    t.index ["plant_id"], name: "index_plot_plants_on_plant_id"
+    t.index ["plot_id"], name: "index_plot_plants_on_plot_id"
   end
 
   create_table "plots", force: :cascade do |t|
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_10_151435) do
     t.index ["garden_id"], name: "index_plots_on_garden_id"
   end
 
-  add_foreign_key "plot_plants", "plants", column: "plants_id"
-  add_foreign_key "plot_plants", "plots", column: "plots_id"
+  add_foreign_key "plot_plants", "plants"
+  add_foreign_key "plot_plants", "plots"
   add_foreign_key "plots", "gardens"
 end

957ad47def3c20800607529aa59192a4cadeedbe
Lance Nelson <lancen04@gmail.com>
2024-06-10 15:34:59 -0400

    completed most of user story 3, but can definitely touch up on a few things

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,15 @@
 class Garden < ApplicationRecord
    has_many :plots
+   has_many :plants, through: :plots
+
+   def unique_plants
+      plants.where("days_to_harvest < 100").distinct
+   end
+
+   def ordered_plants
+      plants.where("days_to_harvest < 100")
+            .select("plants.*, count(plots.id)")
+            .group("plants.id")
+            .order("count(plots.id) desc")
+   end
 end

127c2989238e21736946b1874f334dcffaa3de1a
Garrett Bowman <garrettbowman248@gmail.com>
2024-06-10 10:49:22 -0500

    TESTT TWO READYY

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :plots, only: [:index]
+  resources :plots_plants, only: [:destroy]
+  
+
 end
--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -21,5 +21,52 @@ RSpec.describe "Plots Index Page" do
                 expect(page).to_not have_content(tomato.name)
             end
         end
+        # User Story 2, Remove a Plant from a Plot
+
+        # As a visitor
+        # When I visit the plots index page
+        # Next to each plant's name
+        # I see a button to remove that plant from that plot
+        # When I click on that button
+        # I'm returned to the plots index page
+        # And I no longer see that plant listed under that plot,
+        # And I still see that plant's name under other plots that is was associated with.
+
+        # Note: you do not need to test for any sad paths or implement any flash messages. 
+        it "I see a button to remove a plant from a plot" do
+            garden = Garden.create!(name: "Turing Community Garden", organic: true)
+            plot_1 = garden.plots.create!(number: 1, size: "Large", direction: "North")
+            plot_2 = garden.plots.create!(number: 2, size: "Large", direction: "South")
+
+            pepper = plot_1.plants.create!(name: "Pepper", description: "Needs sun")
+            cucumber = plot_1.plants.create!(name: "Cucumber", description: "Needs water")
+
+            tomato = plot_2.plants.create!(name: "Tomato", description: "Needs water")
+            pepper_plot2 = plot_2.plants.create!(name: "Pepper", description: "Needs sun")
+
+            visit "/plots"
+
+            within("#plot_1") do
+                expect(page).to have_content(plot_1.number)
+                expect(page).to have_content(pepper.name)
+                expect(page).to have_content(cucumber.name)
+
+                expect(page).to_not have_content(tomato.name)
+
+                click_button "Remove"
+            end
+
+            expect(current_path).to eq("/plots")
+
+            within("#plot_1") do
+                expect(page).to have_content(plot_1.number)
+                expect(page).to_not have_content(pepper.name)
+                expect(page).to have_content(cucumber.name)
+            end
+
+            within("#plot_2") do
+                expect(page).to have_content(pepper_plot2.name)
+            end
+        end
     end
 end

b369268ff6934ad6381ac2098d46868666d975c7
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-10 09:53:50 -0600

    test: plot index page

---
+++ spec/features/plots/index_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Plots index page' do
+  before :each do
+    @garden = Garden.create!(name: 'My Garden', organic: true)
+
+    @Plot1 = @garden.plots.create!(number: 1, size: 'Large', direction: 'North')
+    @Plot2 = @garden.plots.create!(number: 2, size: 'Small', direction: 'South')
+
+    @plant1 = Plant.create!(name: 'Sunflower', description: 'seeds')
+    @plant2 = Plant.create!(name: 'Rose', description: 'are red')
+    @plant3 = Plant.create!(name: 'Violet', description: 'are blue')
+  end
+
+  describe 'As a User' do
+    it 'I see a list of all plot numbers' do
+      visit plots_path(@garden)
+
+      expect(page).to have_content(@Plot1.number)
+      expect(page).to have_content(@Plot2.number)
+      expect(page).to_not have_content(@plant1.name)
+      expect(page).to_not have_content(@plant2.name)
+    end
+
+    it 'I see a list of plants associated with each plot' do
+      visit plots_path(@garden)
+      expect(page).to_not have_content(@plant1.name)
+
+      PlantPlot.create!(plant: @plant1, plot: @Plot1)
+      PlantPlot.create!(plant: @plant2, plot: @Plot1)
+
+      expect(page).to have_content(@plant1.name)
+      expect(page).to have_content(@plant2.name)
+      expect(page).to_not have_content(@plant3.name)
+    end
+  end
+end
